3ef630a94827882f13eb5b19cfc93b87c06506a9
fonsiher <ehernandezm@est.ups.edu.ec>
2022-03-24 15:30:29 -0500

    add Heroku connect

--- conexion/conexion.js
+++ conexion/conexion.js
@@ -2,15 +2,15 @@ const { Pool } = require('pg');
 
 const config = {
     // Conexión para la base de datos de Heroku
-    /*     connectionString: 'postgres://kxxlgecbpcpshw:db6f64562de83034c96e962ae82267f74feed602afe73eebcc8895b27badbd94@ec2-54-145-224-156.compute-1.amazonaws.com:5432/dcak7rmvtrl7cp',
-        ssl: { rejectUnauthorized: false }
-     */
+    connectionString: 'postgres://kxxlgecbpcpshw:db6f64562de83034c96e962ae82267f74feed602afe73eebcc8895b27badbd94@ec2-54-145-224-156.compute-1.amazonaws.com:5432/dcak7rmvtrl7cp',
+    ssl: { rejectUnauthorized: false }
+
 
     // Conexión para la base de datos local
-    user: 'postgres',
-    host: 'localhost',
-    password: '123',
-    database: 'Invirtual_Store'
+    // user: 'postgres',
+    // host: 'localhost',
+    // password: 'sololdu',
+    // database: 'InVirtual'
 };
 const pool = new Pool(config)
 
